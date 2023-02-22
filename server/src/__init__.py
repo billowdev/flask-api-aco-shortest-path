74289856ce4fb66de0aa56ac879d2aab2cd0415d
Akkarapon Phikulsri <lacakira@gmail.com>
2023-02-22 18:15:22 +0700

    Update __init__.py

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -11,18 +11,8 @@ from .database.seeders import seeders
 from flask_jwt_extended import JWTManager
 
 def create_app():
-    app = Flask(__name__,
-                instance_relative_config=True)
-    # if test_config is None:
-    # 	app.config.from_mapping(
-    # 		SECRET_KEY=os.environ.get("SECRET_KEY"),
-    # 		SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DB_URI"),
-    # 		SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    # 		# JWT_SECRET_KEY=os.environ.get('JWT_SECRET_KEY')
-    # 	)
-    # else:
-    # 	app.config.from_mapping(test_config)
-    
+    app = Flask(__name__,instance_relative_config=True)
+
     # set up database URI based on the environment
     if app.config['ENV'] == 'development':
         app.config.from_mapping(
@@ -52,9 +42,10 @@ def create_app():
     db.app = app
     db.init_app(app)
     JWTManager(app)
-
+    
     app.register_blueprint(building_bp)
     app.register_blueprint(user_bp)
+    
     # create database tables
     with app.app_context():
         if app.config['ENV'] == 'development':
